8b3d62a3b5176d2832380ce9cfd163f6c5129e3e
David Ward <davidward.info@gmail.com>
2021-02-26 20:23:11 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,14 @@
 # code your #position_taken? method here!
+def index
+  input.to_i - 1
+end
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ",]
+
+def position_taken?(board, index)
+  if index == " "
+    false
+  elsif index == "X" || "O"
+    true
+  end
+end
